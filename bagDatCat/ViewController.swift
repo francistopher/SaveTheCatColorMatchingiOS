2ac7859deabe1f8dbdbd3308e8564dc15aa34412
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-02-06 14:05:12 -0500

    Create referencing outlet for Main.storyboard View Controller in ViewController.swift

--- bagDatCat/ViewController.swift
+++ bagDatCat/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
